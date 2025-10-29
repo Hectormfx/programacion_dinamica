60b3212ea900befa8699515a706cde15e3738016
Hectormfx <hectormfx10@gmail.com>
2025-10-28 21:16:28 -0700

    Create clase03_estructuradedatos.cpp

---
+++ practicas_clase/clase03_estructuradedatos.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+struct Alumno{
+    string nombre;
+    int edad;
+    float promedio;
+};
+
+int main()
+{
+
+    //parametros por valor y por referencia
+    //valor -> copia del original
+    //
+}
