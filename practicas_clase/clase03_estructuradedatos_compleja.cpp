b279391bc4c27ff21e52ca8e84181559c15148f2
Hectormfx <hectormfx10@gmail.com>
2025-10-28 21:03:18 -0700

    Practica de estructura de datos

---
+++ practicas_clase/clase03_estructuradedatos_compleja.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+struct Alumno{
+    string nombre;
+    int edad;
+    float promedio;
+};
+
+int main()
+{
+
+    const int TAM = 5;
+    Alumno grupo[TAM]; // Arreglo de una estructura definida por el usario
+    cout << "===Captura los datos del alumno" << endl;
+    for (int i = 0; i < TAM; i++){
+        cout << "\nAlumno" << i +1 << ":" << endl;
+        getline(cin >> ws, grupo[i].nombre);
+        cout << "Edad: ";
+        cin >> grupo[i].edad;
+        cout << "Promedio: ";
+        cin >> grupo[i].promedio;
+    }
+
+    cout << "=== Listado de alumnos ===" << endl;
+    for (int i=0; i < TAM; i++){
+        cout << "\nAlumno " << i +1 << ":" << endl;
+        cout << "Nombre" << grupo[i].nombre;
+        cout << "Edad" << grupo[i].edad;
+        cout << "Promedio" << grupo[i].promedio;
+    }
+
+    return 0;
+}
